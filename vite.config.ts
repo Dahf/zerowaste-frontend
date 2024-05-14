f50528abec0600b5736d51c1d9e92e92c1f50ad4
Dahf <flacezockt@gmail.com>
2024-05-14 21:43:14 +0200

    changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5005,
+    proxy: {
+      "/api" : {
+        target: "https://api.silasbeckmann.de/",
+        changeOrigin: true,
+        secure: true,      
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
 })
