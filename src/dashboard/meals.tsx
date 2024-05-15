0e3b633215f05fb3c8673ca71da486a2f09945ea
Dahf <flacezockt@gmail.com>
2024-05-15 15:27:18 +0200

    yeay

--- src/dashboard/meals.tsx
+++ src/dashboard/meals.tsx
@@ -1,4 +1,3 @@
-import { Formik, Form, Field, FieldArray } from 'formik';
 import {
   FormControl,
   FormLabel,
@@ -15,6 +14,7 @@ import axios from 'axios';
 import { Section } from '../../components/section';
 import { BackgroundGradient } from '../../components/gradients/background-gradient';
 import { PageTransition } from '../../components/motion/page-transition';
+import { Form, FormLayout, SubmitButton } from '@saas-ui/react';
 
 const Meals = () => {
   return (
@@ -22,9 +22,8 @@ const Meals = () => {
         <BackgroundGradient zIndex="-1" />
         <Center height="100%" pt="20">
             <PageTransition width="100%" py="3em">
-                <Formik
-                
-                initialValues={{
+                <Form
+                defaultValues={{
                     name: '',
                     description: '',
                     servingSize: '',
@@ -37,196 +36,135 @@ const Meals = () => {
                     sodium: '',
                     ingredients: [{ name: '', measure: '', quantity: '' }],
                 }}
-                onSubmit={async (values, actions) => {
+                onSubmit={async (params) => {
+                    console.log(params);
                     try {
-                    console.log(values);
-                    const response = await axios.post('/api/meal', values);
+                    const response = await axios.post('/api/meal', params);
                     console.log(response.data);
-                    // Handle successful response
-                    actions.setSubmitting(false);
                     } catch (error) {
                     console.error('Server error:', error.message);
-                    // Handle error response
-                    actions.setSubmitting(false);
                     }
                 }}
                 >
-                {(props) => (
-                    <Form>
-                    <Field name="name">
-                        {({ field, form }) => (
-                        <FormControl isInvalid={form.errors.name && form.touched.name}>
-                            <FormLabel>Name</FormLabel>
-                            <Input {...field} placeholder="Name" />
-                            <FormErrorMessage>{form.errors.name}</FormErrorMessage>
-                        </FormControl>
-                        )}
-                    </Field>
+                {({ Field, ArrayField }) => (
+                    <FormLayout>
+                    <Field
+                        name="name"
+                        label="Name"
+                        type="text"
+                        help="Choose a name for this project"
+                        rules={{ required: true }}
+                    />
 
-                    <Field name="description">
-                        {({ field, form }) => (
-                        <FormControl isInvalid={form.errors.description && form.touched.description}>
-                            <FormLabel>Description</FormLabel>
-                            <Input {...field} placeholder="Description" />
-                            <FormErrorMessage>{form.errors.description}</FormErrorMessage>
-                        </FormControl>
-                        )}
-                    </Field>
+                    <Field
+                        name="description"
+                        type="textarea"
+                        label="Description"
+                        placeholder="Optional description"
+                    />
 
-                    <Field name="servingSize">
-                        {({ field, form }) => (
-                        <FormControl isInvalid={form.errors.servingSize && form.touched.servingSize}>
-                            <FormLabel>Serving Size</FormLabel>
-                            <Input {...field} placeholder="Serving Size" />
-                            <FormErrorMessage>{form.errors.servingSize}</FormErrorMessage>
-                        </FormControl>
-                        )}
-                    </Field>
+                    <Field
+                        name="servingSize"
+                        label="Serving Size"
+                        type="text"
+                        placeholder="Serving Size"
+                    />
 
                     <SimpleGrid columns={3} spacing={4}>
                         <GridItem colSpan={1}>
-                        <Field name="calories">
-                            {({ field, form }) => (
-                            <FormControl isInvalid={form.errors.calories && form.touched.calories}>
-                                <FormLabel>Calories</FormLabel>
-                                <Input {...field} placeholder="Calories" />
-                                <FormErrorMessage>{form.errors.calories}</FormErrorMessage>
-                            </FormControl>
-                            )}
-                        </Field>
+                        <Field
+                            name="calories"
+                            label="Calories"
+                            type="text"
+                            placeholder="Calories"
+                        />
                         </GridItem>
                         
                         <GridItem colSpan={1}>
-                        <Field name="fat">
-                            {({ field, form }) => (
-                            <FormControl isInvalid={form.errors.fat && form.touched.fat}>
-                                <FormLabel>Fat</FormLabel>
-                                <Input {...field} placeholder="Fat" />
-                                <FormErrorMessage>{form.errors.fat}</FormErrorMessage>
-                            </FormControl>
-                            )}
-                        </Field>
+                        <Field
+                            name="fat"
+                            label="Fat"
+                            type="text"
+                            placeholder="Fat"
+                        />
                         </GridItem>
 
                         <GridItem colSpan={1}>
-                        <Field name="carbohydrates">
-                            {({ field, form }) => (
-                            <FormControl isInvalid={form.errors.carbohydrates && form.touched.carbohydrates}>
-                                <FormLabel>Carbohydrates</FormLabel>
-                                <Input {...field} placeholder="Carbohydrates" />
-                                <FormErrorMessage>{form.errors.carbohydrates}</FormErrorMessage>
-                            </FormControl>
-                            )}
-                        </Field>
+                        <Field
+                            name="carbohydrates"
+                            label="Carbohydrates"
+                            type="text"
+                            placeholder="Carbohydrates"
+                        />
                         </GridItem>
 
                         <GridItem colSpan={1}>
-                        <Field name="protein">
-                            {({ field, form }) => (
-                            <FormControl isInvalid={form.errors.protein && form.touched.protein}>
-                                <FormLabel>Protein</FormLabel>
-                                <Input {...field} placeholder="Protein" />
-                                <FormErrorMessage>{form.errors.protein}</FormErrorMessage>
-                            </FormControl>
-                            )}
-                        </Field>
+                        <Field
+                            name="protein"
+                            label="Protein"
+                            type="text"
+                            placeholder="Protein"
+                        />
                         </GridItem>
 
                         <GridItem colSpan={1}>
-                        <Field name="fiber">
-                            {({ field, form }) => (
-                            <FormControl isInvalid={form.errors.fiber && form.touched.fiber}>
-                                <FormLabel>Fiber</FormLabel>
-                                <Input {...field} placeholder="Fiber" />
-                                <FormErrorMessage>{form.errors.fiber}</FormErrorMessage>
-                            </FormControl>
-                            )}
-                        </Field>
+                        <Field
+                            name="fiber"
+                            label="Fiber"
+                            type="text"
+                            placeholder="Fiber"
+                        />
                         </GridItem>
 
                         <GridItem colSpan={1}>
-                        <Field name="sugar">
-                            {({ field, form }) => (
-                            <FormControl isInvalid={form.errors.sugar && form.touched.sugar}>
-                                <FormLabel>Sugar</FormLabel>
-                                <Input {...field} placeholder="Sugar" />
-                                <FormErrorMessage>{form.errors.sugar}</FormErrorMessage>
-                            </FormControl>
-                            )}
-                        </Field>
+                        <Field
+                            name="sugar"
+                            label="Sugar"
+                            type="text"
+                            placeholder="Sugar"
+                        />
                         </GridItem>
 
                         <GridItem colSpan={1}>
-                        <Field name="sodium">
-                            {({ field, form }) => (
-                            <FormControl isInvalid={form.errors.sodium && form.touched.sodium}>
-                                <FormLabel>Sodium</FormLabel>
-                                <Input {...field} placeholder="Sodium" />
-                                <FormErrorMessage>{form.errors.sodium}</FormErrorMessage>
-                            </FormControl>
-                            )}
-                        </Field>
+                        <Field
+                            name="sodium"
+                            label="Sodium"
+                            type="text"
+                            placeholder="Sodium"
+                        />
                         </GridItem>
                     </SimpleGrid>
 
-                    <FieldArray name="ingredients">
-                        {({ remove, push }) => (
-                        <div>
-                            {props.values.ingredients.length > 0 &&
-                            props.values.ingredients.map((ingredient, index) => (
-                                <div key={index}>
-                                <Field name={`ingredients.${index}.name`}>
-                                    {({ field, form }) => (
-                                    <FormControl isInvalid={form.errors.ingredients?.[index]?.name && form.touched.ingredients?.[index]?.name}>
-                                        <FormLabel>Ingredient Name {index}</FormLabel>
-                                        <Input {...field} placeholder="Ingredient Name" />
-                                        <FormErrorMessage>{form.errors.ingredients?.[index]?.name}</FormErrorMessage>
-                                    </FormControl>
-                                    )}
-                                </Field>
-                                <Field name={`ingredients.${index}.measure`}>
-                                    {({ field, form }) => (
-                                    <FormControl isInvalid={form.errors.ingredients?.[index]?.measure && form.touched.ingredients?.[index]?.measure}>
-                                        <FormLabel>Measure {index}</FormLabel>
-                                        <Input {...field} placeholder="Measure" />
-                                        <FormErrorMessage>{form.errors.ingredients?.[index]?.measure}</FormErrorMessage>
-                                    </FormControl>
-                                    )}
-                                </Field>
-                                <Field name={`ingredients.${index}.quantity`}>
-                                    {({ field, form }) => (
-                                    <FormControl isInvalid={form.errors.ingredients?.[index]?.quantity && form.touched.ingredients?.[index]?.quantity}>
-                                        <FormLabel>Quantity {index}</FormLabel>
-                                        <Input {...field} placeholder="Quantity" />
-                                        <FormErrorMessage>{form.errors.ingredients?.[index]?.quantity}</FormErrorMessage>
-                                    </FormControl>
-                                    )}
-                                </Field>
-                                <Button type="button" onClick={() => remove(index)}>
-                                    Remove
-                                </Button>
-                                </div>
-                            ))}
-                            <Button type="button" onClick={() => push({ name: '', measure: '', quantity: '' })}>
-                            Add Ingredient
-                            </Button>
-                        </div>
-                        )}
-                    </FieldArray>
+                    <ArrayField name="ingredients">
+                        <Field
+                            name={`ingredients.$.name`}
+                            label={`Ingredient Name $`}
+                            type="text"
+                            placeholder="Ingredient Name"
+                        />
+                        <Field
+                            name={`ingredients.$.measure`}
+                            label={`Measure $`}
+                            type="text"
+                            placeholder="Measure"
+                        />
+                        <Field
+                            name={`ingredients.$.quantity`}
+                            label={`Quantity $`}
+                            type="text"
+                            placeholder="Quantity"
+                        />
+                    </ArrayField>
 
                     <HStack>
-                        <Button
-                        mt={4}
-                        colorScheme="primary"
-                        isLoading={props.isSubmitting}
-                        type="submit"
-                        >
+                        <SubmitButton>
                         Submit
-                        </Button>
+                        </SubmitButton>
                     </HStack>
-                    </Form>
+                    </FormLayout>
                 )}
-                </Formik>
+                </Form>
+
             </PageTransition>
         </Center>
     </Section>
