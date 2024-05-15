bf78048e68d94d5fcfab60a9ab4bbdcdb2cc90e5
Dahf <flacezockt@gmail.com>
2024-05-15 14:21:20 +0200

    added preview

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,17 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 5005,
+    proxy: {
+      '/api': {
+        target: 'http://zerowaste-api:8088', // Die URL von Traefik, die auf das Backend weiterleitet
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   server: {
     host: true,
     port: 5005,
